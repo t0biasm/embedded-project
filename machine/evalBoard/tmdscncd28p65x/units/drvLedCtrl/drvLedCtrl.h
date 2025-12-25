0fd156e4d18c7d0310c29dfa3f82262cc8468317
Tobias Maier <maier-tobias@gmx.de>
2025-12-25 12:33:15 +0100

    Add Header File Structure Organization

--- machine/evalBoard/tmdscncd28p65x/units/drvLedCtrl/drvLedCtrl.h
+++ machine/evalBoard/tmdscncd28p65x/units/drvLedCtrl/drvLedCtrl.h
@@ -1,3 +1,58 @@
-#include "public_types.h"
+/**
+ * @file drvLedCtrl.h
+ * @author Tobias Maier (maier-tobias@gmx.de)
+ * @brief External data of LED control driver component
+ * @version 0.1
+ * @date 2025-12-25
+ *
+ * @copyright Copyright (c) 2025
+ *
+ */
 
+#ifndef DRVLEDCTRL_H
+#define DRVLEDCTRL_H
+
+/* ---------------------------------------- Includes --------------------------------------------*/
+#pragma region
+#include "public_types.h"
+#pragma endregion /* Includes */
+
+/* ----------------------------------------- Defines --------------------------------------------*/
+#pragma region
+
+#pragma endregion /* Defines */
+
+/* ---------------------- ---------- Global Type Definitions ------------------------------------*/
+#pragma region
+#pragma region    /* Enumerations */
+
+#pragma endregion /* Enumerations */
+
+#pragma region    /* Typedefs */
+
+#pragma endregion /* Typedefs */
+
+#pragma region    /* Structs */
+
+#pragma endregion /* Structs */
+
+#pragma region    /* Unions */
+
+#pragma endregion /* Unions */
+#pragma endregion /* Global Type Definitions */
+
+/* ------------------------------ Global Variable Declarations ----------------------------------*/
+#pragma region
+
+#pragma endregion /* Global Variable Declarations */
+
+/* ----------------------------------- Function Prototypes ------------------------------------- */
+#pragma region    /* Global Function Prototypes */
 extern void gfDrvLedCtrl_10ms(void);
+#pragma endregion /* Global Function Prototypes */
+
+#pragma region    /* External Function Prototypes */
+
+#pragma endregion /* External Function Prototypes */
+
+#endif            /* DRVLEDCTRL_H */
